b5fc1b23d99b9d79e6bc900ee74332bcc1265246
Stefano Chiapparini <chiapparinistefano@gmail.com>
2025-06-17 16:36:46 +0200

    ch1

---
+++ message.py
@@ -0,0 +1,8 @@
+print(2+6)
+print(2*4.)
+print(16/2)
+print(9-1)
+
+#This program asks the user his fav number and return a print 
+fav_num =input('What is your favorite number?')
+print(f'My favourite number is: {fav_num}')
