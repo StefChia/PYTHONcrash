c81cefa361113c516b7e461fdc22e79160c5b757
Stefano Chiapparini <chiapparinistefano@gmail.com>
2025-07-17 08:05:18 +0200

    gg

--- projects/alien_invasion/settings.py
+++ projects/alien_invasion/settings.py
@@ -4,6 +4,7 @@ class Settings:
     #screen settings
     def __init__(self):
         "Initialize attributes"
+        self.ship_speed = 1.5
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
